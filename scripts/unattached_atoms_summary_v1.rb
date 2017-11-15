19f7b7ee45ba1d083ef86d933d129f70b263e55d
Andy Gallagher <andy.gallagher@theguardian.com>
2017-11-15 13:27:45 +0000

    csv output for script, plus customisable sorting

--- scripts/unattached_atoms_summary_v1.rb
+++ scripts/unattached_atoms_summary_v1.rb
@@ -17,20 +17,22 @@ def get_date_range(item_list, field_name)
   rtn
 end
 
-def user_list(item_list)
+def user_list(item_list, sortby)
   individual_users = item_list.map{|item| item['userEmail']}.uniq
   unsorted_list = individual_users.map do |username|
     {:user=>username,
      :items=>item_list.select{|item| item['userEmail']==username}.length
     }
   end
-  unsorted_list.sort{|a,b| a[:items] <=> b[:items]}.reverse
+  unsorted_list.sort{|a,b| a[sortby] <=> b[sortby]}.reverse
 end
 
 #START MAIN
 opts = Trollop::options do
   opt :region, "AWS region to talk to", :type=>:string, :default=>'eu-west-1'
   opt :environment, "Talk to CODE or PROD?", :type=>:string, :default=>'PROD'
+  opt :csv, "Output as CSV rather than a summary", :type=>:boolean, :default=>false
+  opt :sortuser, "Sort by username rather than item count", :type=>:boolean, :default=>false
 end
 
 client = Aws::DynamoDB::Client.new(:region=>opts.region)
@@ -47,10 +49,26 @@ item_list = result.items.map{ |item|
   new_hash
 }
 
-ap item_list
+#ap item_list
 created_date_range = get_date_range(item_list,"dateCreated")
 created_date_range[:earliest] = created_date_range[:earliest].iso8601
 created_date_range[:latest] = created_date_range[:latest].iso8601
 
-puts "Got a total of #{result.count} items in the range #{created_date_range}, scanned #{result.scanned_count}"
-ap user_list(item_list)
+if opts.sortuser
+  sortby = :user
+else
+  sortby = :items
+end
+
+if opts.csv
+  puts "Range start\t#{created_date_range[:earliest]}\tRange end\t#{created_date_range[:latest]}"
+  puts "Total items in timespan\t#{result.count}\tIncluded items\t#{result.scanned_count}"
+  puts "User\tUnattached items"
+
+  user_list(item_list, sortby).each {|userentry|
+    puts "#{userentry[:user]}\t#{userentry[:items]}"
+  }
+else
+  puts "Got a total of #{result.count} items in the range #{created_date_range}, scanned #{result.scanned_count}"
+  ap user_list(item_list, sortby)
+end
