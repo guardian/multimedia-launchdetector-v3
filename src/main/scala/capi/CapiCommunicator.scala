fdefbae2d4883f5dfcc81959ffb50169a8776ed2
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-08-05 14:50:33 +0100

    Use rate-limited endpoint

--- src/main/scala/capi/CapiCommunicator.scala
+++ src/main/scala/capi/CapiCommunicator.scala
@@ -1,6 +1,7 @@
 package capi
 
 import com.gu.contentapi.client.model._
+import com.gu.contentapi.client.GuardianContentClient
 import java.time.Instant
 
 import com.gu.contentapi.client.model.v1.ItemResponse
@@ -12,7 +13,7 @@ import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 trait CapiCommunicator {
-  private val client = new GuardianContentClientInternal("multimedia-launchdetector")
+  def client: GuardianContentClient
 
   def lookupAtom(atomId:String, atomType:String):Future[Option[Atom]] = {
     val atomQuery = ItemQuery(s"atom/$atomType/$atomId")
